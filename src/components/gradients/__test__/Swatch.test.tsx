63cd417f592086402b97cc3065d596d3a3779fdb
Phil Zona <phil.b.zona@gmail.com>
2022-11-24 10:14:51 -0500

    Add test for swatch

--- src/components/gradients/__test__/Swatch.test.tsx
+++ src/components/gradients/__test__/Swatch.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import convert from 'color-convert';
 import Swatch from '../Swatch';
+import { RGB } from 'color-convert/conversions';
 
 test('renders swatch component', () => {
-  // render(<Swatch />);
-  // const title = screen.getByText(/gradiant/i);
-  // expect(title).toBeInTheDocument();
+  const testColor = '2476FF';
 
-  return undefined;
-});
+  const { container } = render(<Swatch color={'#'+testColor} />);
+  const bgColor = (container.firstChild as HTMLElement).style.backgroundColor;
+  // @ts-ignore
+  const bgColorToHex = convert.rgb.hex(bgColor.match(/\d+/g));
+
+  expect(bgColorToHex).toEqual(testColor);
+})
