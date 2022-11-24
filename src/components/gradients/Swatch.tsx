c91e027e71c1b566d1754f22f75781cea22f9a2f
Phil Zona <phil.b.zona@gmail.com>
2022-11-23 21:14:49 -0500

    Add swatch component

---
+++ src/components/gradients/Swatch.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Swatch.module.css';
+
+interface SwatchProps {
+  color: string;
+}
+
+const Swatch = ({ color }: SwatchProps) => (
+  <div className={styles.swatch} style={{ backgroundColor: color }}/>
+);
+
+export default Swatch;
