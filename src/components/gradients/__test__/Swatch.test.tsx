4c4ceb0cdddec48e2d0d870155ff5b3fb2980b3d
Phil Zona <phil.b.zona@gmail.com>
2022-11-23 21:16:12 -0500

    Add test for swatch

---
+++ src/components/gradients/__test__/Swatch.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Swatch from '../Swatch';
+
+test('renders swatch component', () => {
+  // render(<Swatch />);
+  // const title = screen.getByText(/gradiant/i);
+  // expect(title).toBeInTheDocument();
+
+  return undefined;
+});
