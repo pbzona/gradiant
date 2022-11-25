e674357d8b16f6250e0bc571fc10c23ad883ce84
Phil Zona <phil.b.zona@gmail.com>
2022-11-24 20:08:12 -0500

    Add comment about the swatch test

--- src/components/gradients/__test__/Swatch.test.tsx
+++ src/components/gradients/__test__/Swatch.test.tsx
@@ -3,7 +3,12 @@ import { render } from '@testing-library/react';
 import convert from 'color-convert';
 import Swatch from '../Swatch';
 
-test('renders swatch component', () => {
+test('renders swatch component with correct backgroundColor', () => {
+  // Kind of a weird test - the purpose is to check the background color is set properly
+  // The thing here is that we will usually (always?) pass the color value as a hex, but
+  // it gets converted to rgb implicitly so I need to also do that conversion to
+  // test whether the background color has been set correctly
+
   const testColor = '2476FF';
 
   const { container } = render(<Swatch color={'#'+testColor} />);
