3eacaac942abc60fa6349196b7db7cb9906c128d
Phil Zona <phil.b.zona@gmail.com>
2022-12-01 18:00:48 -0500

    Add shared UI styles css module

--- src/components/ui/ColorPicker.tsx
+++ src/components/ui/ColorPicker.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEventHandler } from 'react';
 import styles from './ColorPicker.module.css';
+import uiStyles from './UIStyles.module.css';
 
 interface IColorPickerProps {
   label: string;
@@ -11,7 +12,7 @@ export const ColorPicker = ({ label, value, onChange}: IColorPickerProps) => {
   return (
     <div className={styles.container}>
       <input type='color' id={label} name={label} value={value} onChange={onChange}/>
-      <label className={styles.label} htmlFor={label}>{label}</label>
+      <label className={uiStyles.label} htmlFor={label}>{label}</label>
     </div>
   );
 };
