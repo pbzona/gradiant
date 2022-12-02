62a4e5b851a6749124bb4b602d4798e53dd647f5
Phil Zona <phil.b.zona@gmail.com>
2022-12-02 14:34:41 -0500

    Add test for color picker input type

--- src/components/ui/__test__/ColorPicker.test.tsx
+++ src/components/ui/__test__/ColorPicker.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
 import { render, screen } from '@testing-library/react';
 import ColorPicker from '../ColorPicker';
 import Config from '../../../config/Config';
 
 const defaultValue = Config.defaultGradientStart.hex;
+
+test('renders an input with type color', (() => {
+  const div = document.createElement('div');
+
+  act(() => {
+    const root = createRoot(div!);
+    root.render(<ColorPicker label='ColorPickerTest' value={defaultValue} onChange={() => null} />);
+  });
+
+  expect(div.querySelector('input')).toHaveAttribute('type', 'color');
+}));
 
 test('renders color picker with correct label', () => {
   render(<ColorPicker label='ColorPickerTest' value={defaultValue} onChange={() => null} />);
